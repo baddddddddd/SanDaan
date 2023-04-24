174b5e08d3251f895ef47bea117fa1ceb7049462
baddddddddd <eluxeofficial@gmail.com>
2023-04-24 22:08:10 +0800

    Apply FloatLayout for MainScreen

--- src/main.py
+++ src/main.py
@@ -1,22 +1,21 @@
-from kivy.core.text import LabelBase
 from kivy.clock import Clock
+from kivy.core.text import LabelBase
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager
-from kivy_garden.mapview import MapMarker, MapView
+from kivy.utils import platform
 from kivymd.app import MDApp
 from kivymd.uix.button import MDRoundFlatIconButton, MDIconButton
+from kivymd.uix.floatlayout import FloatLayout
 from kivymd.uix.screen import MDScreen
+from kivy_garden.mapview import MapMarker, MapView
 from plyer import gps
 import bcrypt
-
-from android.permissions import request_permissions, Permission
 
 
 class InteractiveMap(MapView):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        self.current_location = [13.78669, 121.07459]
         self.current_location_pin = MapMarker(
             lat=self.current_location[0],
             lon=self.current_location[1],
@@ -28,7 +27,7 @@ class InteractiveMap(MapView):
         self.add_widget(self.current_location_pin)
         self.add_widget(self.target_location_pin)
 
-
+    ################## Experimenting
         self.touch_event = None
 
 
@@ -67,16 +66,12 @@ class InteractiveMap(MapView):
             self.target_location_pin.lon = self.target_location.lon
 
             self.add_widget(self.target_location_pin)
-            
-        
+    ##############################        
 
-class MapViewScreen(MDScreen):
-    def __init__(self, **kw):
-        super().__init__(**kw)
 
-        request_permissions([Permission.ACCESS_FINE_LOCATION, Permission.ACCESS_COARSE_LOCATION])
-        gps.configure(on_location=self.update_location)
-        gps.start()
+class MainScreenLayout(FloatLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
         self.has_initialized_gps = False
 
@@ -84,6 +79,26 @@ class MapViewScreen(MDScreen):
         self.mapview = InteractiveMap()
         self.add_widget(self.mapview)
 
+        if platform == "android":
+            from android.permissions import request_permissions, Permission
+
+            request_permissions([Permission.ACCESS_FINE_LOCATION, Permission.ACCESS_COARSE_LOCATION])
+            
+            gps.configure(on_location=self.update_location)
+            gps.start()
+            
+        else:
+            self.has_initialized_gps = True
+
+            self.current_location = [13.78530, 121.07339]
+            self.mapview.lat = self.current_location[0]
+            self.mapview.lon = self.current_location[1]
+            self.mapview.zoom = 15
+
+            self.mapview.current_location_pin.lat = self.current_location[0]
+            self.mapview.current_location_pin.lon = self.current_location[1]
+
+        ################### Experimenting
         self.directions_button = MDIconButton()
 
         self.directions_button.pos_hint = {
@@ -97,6 +112,7 @@ class MapViewScreen(MDScreen):
         self.directions_button.text_color = [26, 24, 58, 255]
 
         self.add_widget(self.directions_button)
+        ####################################
 
 
     def update_location(self, **kwargs):
@@ -111,6 +127,14 @@ class MapViewScreen(MDScreen):
         self.mapview.current_location_pin.lon = kwargs["lon"]
 
 
+class MapViewScreen(MDScreen):
+    def __init__(self, **kw):
+        super().__init__(**kw)
+
+        self.layout = MainScreenLayout()
+        self.add_widget(self.layout)
+
+
 class MainApp(MDApp):
     def build(self):
         screen_manager = ScreenManager()
@@ -120,8 +144,6 @@ class MainApp(MDApp):
         screen_manager.add_widget(Builder.load_file("screens/signup.kv"))
 
         return screen_manager
-    
-    
     
 
 if __name__ == "__main__":
