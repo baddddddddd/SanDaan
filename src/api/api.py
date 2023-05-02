960ca0301669c9a64f85a63e17524c60941134a7
Alexgillol <apenuliar75@gmail.com>
2023-05-02 19:30:12 +0800

    Update api.py

--- src/api/api.py
+++ src/api/api.py
@@ -1,13 +1,15 @@
 from flask import Flask, jsonify, request, session
-
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token
+from flask_bcrypt import bcrypt
 import mysql.connector
 import os
 import osmnx as ox
 import networkx as nx
 
 app = Flask(__name__)
-
 app.secret_key = os.urandom(24)
+jwt = JWTManager(app)
+bcrypt = bcrypt(app)
 
 db = mysql.connector.connect(
     host=os.getenv("HOST"),
@@ -20,13 +22,16 @@ cursor = db.cursor()
 
 @app.route('/register', methods=['POST'])
 def register():
-    json = request.json
-    email = json['email']
-    user = json['username']
-    pswd = json['password']
-    if email and user and pswd and request.method == 'POST':
-        query = "INSERT INTO login(email, username, password) VALUES(%s, %s, %s)"
-        data = (email, user, pswd)            
+
+    id = request.json['user_id']
+    email = request.json.get['email']
+    user = request.json.get['username']
+    pswd = request.json.get['password']
+    salt = bcrypt.gensalt()
+    hashed_pswd = bcrypt.generate_password_hash((pswd + salt).encode('utf-8')).decode('utf-8')
+    if id and email and user and hashed_pswd and salt and request.method == 'POST':
+        query = ("INSERT INTO users(user_id, email, username, password, salt) VALUES(%s, ?, ?, ?)", (email, user, hashed_pswd, salt))
+        data = (email, user, pswd, id)            
         db.execute(query, data)
         cursor.commit()
         response = jsonify('REGISTERED SUCCESSFULLY')
@@ -35,47 +40,40 @@ def register():
     else:
         return showMessage()
     
-@app.route('/login', methods=['GET'])
+@app.route('/login', methods=['POST'])
 def login():
-    if 'loggedin' in session:
-        return jsonify('User is already logged in')
-    
-    email = request.form['email']
-    password = request.form['password']
+    user = request.json.get('username')
+    pswd = request.json.get('password')
 
-    if email and password:
-        cursor.execute('SELECT * FROM users WHERE email = %s AND password = %s', (email, password))
-        user = cursor.fetchone()
-        if user:
-            session['loggedin'] = True
-            session['id'] = user[0]
-            session['email'] = user[1]
-            session['username'] = user[2]
-            return jsonify('Logged in successfully')
+    result = db.execute("SELECT salt FROM users WHERE username = ?", (user,))
+    user = result.fetchone()
+
+    if user:
+        hashed_pswd = user['password']
+        salt = user['salt']
+        hashed_input_password = bcrypt.generate_password_hash((pswd + salt).encode('utf-8'))
+
+        if bcrypt.check_password_hash(hashed_pswd, hashed_input_password):
+            access_token = create_access_token(identity=user)
+            return jsonify(access_token=access_token), 200
         else:
-         return jsonify('Invalid email or password')
+            return jsonify({'message': 'Invalid credentials'}), 401
     else:
-        return jsonify('Email and password are required')
-@app.route('/update', methods=['PUT'])
-def update_login():
-    try:
-        json = request.json
-        id = json['user_id']
-        email = json['email']
-        user = json['username']
-        pswd = json['password']
-        if email and user and pswd and request.method == 'POST':
-            query = "UPDATE users SET email=%s, username=%s, password=%s WHERE user_id=%s"
-            data = (id, email, user, pswd)
-            db.execute(data, query)
-            cursor.commit()
-            response = jsonify('UPDATED SUCCESSFULLY')
-            response.status_code = 200
-            return response
-        else:
-            return showMessage()
-    except Exception as e:
-        print(e)
+        return jsonify({'message': 'User not found'}), 404
+    
+@app.route('/protected', methods=['GET'])
+@jwt_required()
+def protected():
+    user_id = jwt.get_jwt_identity()
+
+    cursor = db.cursor()
+    query = "SELECT user_id, username FROM users WHERE user_id = %s"
+    data = (user_id,)
+    cursor.execute(query, data)
+    user = cursor.fetchone()
+
+    return jsonify({'user': {'id': user[0], 'username': user[1]}}), 200
+
 @app.errorhandler(404)
 def showMessage(error=None):
     message = {
@@ -85,30 +83,6 @@ def showMessage(error=None):
     response.status_code = 404
     return response
 
-@app.route('/logout')
-def logout():
-    session.pop('email', None)
-    return "Logged out successfully"
-
-@app.route('/protected')
-def protected():
-    if 'email' in session:
-        return "This is a protected route"
-    else:
-        return "You are not logged in"
-
-@app.route("/account", methods=["GET"])
-def manage_account():
-    if request.method == "GET":
-        cursor.execute("SELECT * FROM users WHERE username=\"eluxe\"")
-        user = cursor.fetchone()
-        print(user)
-        return "done"
-
-    elif request.method == "POST":
-        pass
-
-        
 
 @app.route("/directions", methods=["POST"])
 def get_directions():
